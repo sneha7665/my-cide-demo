96859024c6de9a0f4b4d0010853452627c757def
Sneha Airodagi <airodagisneha@gmail.com>
2026-07-23 14:48:54 +0530

    Create a Flask application with a simple homepage

---
+++ application.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template_string
+
+application = Flask(__name__)
+
+HTML = """
+<!DOCTYPE html>
+<html>
+<head><title>My CI/CD App</title></head>
+<body>
+    <h1>Hello from Elastic Beanstalk! DEPLOYED!</h1>
+    <p>This was deployed automatically via CodePipeline</p>
+</body>
+</html>
+"""
+
+@application.route('/')
+def home():
+    return render_template_string(HTML)
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=8080)
